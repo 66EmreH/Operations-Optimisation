53edf484a1cde1337cfbb51a99f48a95d7d395ac
Ellesyang <e.h.f.yang@student.tudelft.nl>
2025-12-03 11:18:19 +0100

    added variables

---
+++ Variables.py
@@ -0,0 +1,9 @@
+F = {}
+A = {} #set of arrival flights
+D = {} #set of departure flights
+ai = {} #scheduled arrival time i
+di = {} #scheduled departure time for flight i 
+W = {} #set of aprons
+G = {} #set of gates, Gn+1 is remote gate set, G0 is contact gate set
+K = {} #set of gate types
+
