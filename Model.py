61a3b66c45ff7d0df8f0187e996fdeb0fdb50c5e
trpvanderpaardt <timovanderpaardt@gmail.com>
2025-12-29 13:29:35 +0100

    Model update

--- Model.py
+++ Model.py
@@ -1,3 +1,49 @@
-from Data import flights, Gates
+from Data import flights
+import gurobipy as gp
+from Variables import F, G, Gamma, K, A, F_k
+
+f = len(F) #number of flights
+g = len(G) #number of gates
+gamma = len(Gamma) #number of runways
+k = len(K) #number of gate types
+a = len(A) #number of arriving filghts
+d = len(D) #number of departing flights
+fk = len(F_k) #number of flights allowed at gate type k
+
+
+m = gp.Model('Gate_Assignment')
+
+# --------------------------------------
+# Decision variables
+# --------------------------------------
+
+
+X_ijh = m.addVar(shape=(f, f, g), vtype=gp.GRB.BINARY, name='flights_i_and_j_are_assigned_to_gate_h_successively')
+y_igamma =m.addVar(shape=(gamma, f), vtype=gp.GRB.BINARY, name='flight_i_takes_off_on_runway_gamma')
+
+
+# --------------------------------------
+# Constraints
+# --------------------------------------
+
+
+
+#13
+m.addConstrs(gp.quicksum(
+    X_ijh[i][j][h]
+for j in range
+)
+for i in range(a)
+
+)
+
+#14
+m.addConstrs(gp.quicksum(
+    y_igamma[gamma][i]
+for gamma in range(gamma))== 1
+for i in range(d)
+)
+
+#15
 
 
