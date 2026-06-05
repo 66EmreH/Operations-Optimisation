4a8d57d2c96a09267a5fbab4af9491d9f41d8341
trpvanderpaardt <timovanderpaardt@gmail.com>
2026-06-05 10:10:58 +0200

    Fixed arrivals

--- Arrival_checker.py
+++ Arrival_checker.py
@@ -19,42 +19,31 @@ print(runway_3_sorted)
 
 def check_arrival_conflicts(df):
     runway_3 = df[df['arrival_runway'] == 3]
-    df1 = runway_3.sort_values(by='arrival_time')
+    df1 = runway_3.sort_values(by='arrival_time').copy()
     runway_4 = df[df['arrival_runway'] == 4]
-    df2 = runway_4.sort_values(by='arrival_time')
-    swapped = 0
+    df2 = runway_4.sort_values(by='arrival_time').copy()
+    changed = 0
     for i in range(len(df1) - 1):
         i = i+1
-        if df1.iloc[i]['arrival_time'] < df1.iloc[i-1]['arrival_time'] + 2:
-            df1.iloc[i]['arrival_runway'] = 4
-            swapped = swapped + 1
-    print(f"Number of conflicts detected on runway {df1.iloc[i]['arrival_runway']}: {swapped}")
+        if df1.iloc[i]['arrival_time'] < df1.iloc[i-1]['arrival_time'] + 1:
+            df1.loc[df1.index[i], 'arrival_time'] = df1.iloc[i]['arrival_time'] + 1
+        elif df1.iloc[i]['arrival_time'] < df1.iloc[i-1]['arrival_time'] + 2 and df1.iloc[i]['aircraft_size'] in ['B', 'C'] and df1.iloc[i-1]['aircraft_size'] in ['D', 'E', 'F']:
+            df1.loc[df1.index[i], 'arrival_time'] = df1.iloc[i]['arrival_time'] + 1
+            changed = 1
     for i in range(len(df2) - 1):
         i = i+1
-        if df2.iloc[i]['arrival_time'] < df2.iloc[i-1]['arrival_time'] + 2:
-            df2.iloc[i]['arrival_runway'] = 3
-            swapped = swapped + 1
-    print(f"Number of conflicts detected on runway {df2.iloc[i]['arrival_runway']}: {swapped}")
-    dfnew = pd.concat([df1, df2]).sort_values(by='arrival_time')
+        if df2.iloc[i]['arrival_time'] < df2.iloc[i-1]['arrival_time'] + 1:
+            df2.loc[df2.index[i], 'arrival_time'] = df2.iloc[i]['arrival_time'] + 1
+        elif df2.iloc[i]['arrival_time'] < df2.iloc[i-1]['arrival_time'] + 2 and df2.iloc[i]['aircraft_size'] in ['B', 'C'] and df2.iloc[i-1]['aircraft_size'] in ['D', 'E', 'F']:
+            df2.loc[df2.index[i], 'arrival_time'] = df2.iloc[i]['arrival_time'] + 1
+            changed = 1    
+    return df1, df2, changed
 
-    return swapped, dfnew
-going = True
-timer = 20
-previous_swapped = 1e6
-while going:
-
-    swapped, df = check_arrival_conflicts(df)
-    if previous_swapped <= swapped:
-        timer -= 1
-    previous_swapped = swapped
-    final_df = df.sort_values(by='arrival_time')
-
-    if timer == 0:
-        print("No more improvements detected. Final schedule:")
-        print(final_df)
+while True:
+    runway_3_sorted, runway_4_sorted, changed = check_arrival_conflicts(df)
+    df = pd.concat([runway_3_sorted, runway_4_sorted], ignore_index=True)
+    if changed == 0:
         break
 
-    if swapped == 0:
-        print("No more conflicts detected. Final schedule:")
-        print(final_df)
-        break
+
+df.to_excel('paper_case_manuel_instance_updated.xlsx', index=False)
